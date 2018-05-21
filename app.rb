ce61b18b267675924113833b16b2bbe156289325
Jayon Kim <jpkim921@gmail.com>
2018-05-21 03:52:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,8 @@ class App < Sinatra::Base
 
   post '/reverse' do
 
-    puts params[:string].reverse
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
     erb :reversed
   end
 
